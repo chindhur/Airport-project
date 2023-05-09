928478965eb76a8ddbe0e76be37882af1d0b2ed3
chindhu Rathnasamy <crathnasamy.laba@solvd.com>
2023-05-09 15:42:12 -0700

    added case sensitive logic

--- src/main/java/com/utils/UniqueWords.java
+++ src/main/java/com/utils/UniqueWords.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -15,16 +16,19 @@ public class UniqueWords {
 
             //open the file with stream and get all lines
             try (Stream<String> lines = Files.lines(
-                    Path.of("C:\\Documents\\solvd\\intellij\\filereader.txt.txt")))
+                    Path.of("C:\\Documents\\solvd\\intellij\\Airport-project\\src\\main\\resources\\filereader.txt.txt")))
             {
                 //filter each line into words ,splitting by space
-                Stream<String> words = lines
-                        .flatMap(line -> Stream.of(line.split("\\W+")));
+                Set<String> words = lines
+                        .flatMap(line -> Stream.of(line.split("\\W+")))
+                        .map(String::toLowerCase)
+                        .collect(Collectors.toSet());
+                Printer.print("The unique words count is :"+words.size());
 
-                Set<String> wordSet = words.collect(Collectors.toSet());
-                FileWriter fileWriter = new FileWriter("C:\\Documents\\solvd\\intellij\\uniqwords.txt");
+                FileWriter fileWriter = new FileWriter
+                        ("C:\\Documents\\solvd\\intellij\\Airport-project\\src\\main\\resources\\uniqwords.txt");
                 PrintWriter printWriter = new PrintWriter(fileWriter);
-                printWriter.print(wordSet);
+                printWriter.print(words);
                 printWriter.close();
             }
         }
